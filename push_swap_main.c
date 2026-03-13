ca1979df92a28791ba847c54c509e5bebac4d60a
Dna9a <youssefabied12@gmail.com>
2026-03-13 17:03:45 +0000

    stack

--- push_swap_main.c
+++ push_swap_main.c
@@ -2,7 +2,11 @@
 
 int main(int argc, char **argv)
 {
+	t_stack *stack_a;
+	t_stack *stack_b;
 	if (argc < 2)
 		return (0);
+	stack_a = start_stack(argc - 1);
+	stack_b = start_stack(argc - 1);
 	return (0);
 }
