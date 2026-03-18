d0a1a1f4e24c0ba296e2a8813fa339c729ac138b
Dna9a <youssefabied12@gmail.com>
2026-03-18 20:08:11 +0000

    push swap

--- parce.c
+++ parce.c
@@ -1,15 +1,12 @@
 #include "push_swap.h"
 
-/*
-The parser needs to check 3 things:
-✅ Only digits (and maybe a - or + sign at the start)
-✅ No duplicates
-✅ No integer overflow (number fits in an int)
-*/
-
-int	ft_is_valid_number(char *str)
+int	ft_number(char *str)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
+	if (!str || str[0] == '\0')
+		return (0);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
 	while (str[i])
@@ -19,4 +16,78 @@ int	ft_is_valid_number(char *str)
 		i++;
 	}
 	return (1);
+}
+
+int	ft_double(char **argv, int argc)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+long	ft_atol(const char *str)
+{
+	long			s;
+	unsigned long	r;
+	size_t			i;
+
+	s = 1;
+	r = 0;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			s = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		r = r * 10 + (str[i] - '0');
+		i++;
+		if (s == 1 && r > LONG_MAX)
+			return (-1);
+		if (s == -1 && r > (unsigned long)LONG_MAX + 1)
+			return (0);
+	}
+	return ((long)(r * s));
+}
+
+int	ft_is_overflow(char *str)
+{
+	long	n;
+
+	n = ft_atol(str);
+	if (n > 2147483647 || n < -2147483648)
+		return (1);
+	return (0);
+}
+
+void    ft_jma3_o_twi(int argc, char **argv)
+{
+    int i;
+
+    i = 1;
+    if (ft_double(argv, argc))
+		return(write(2, "Error\n", 6); , 0);
+    while (i < argc)
+    {
+        if (!ft_number(argv[i]) || ft_is_overflow(argv[i]))
+			write(2, "Error\n", 6);
+        i++;
+    }
 }
