5be9e93c1fe8eeb6e69dadf440b99aba6957f1b1
Dna9a <youssefabied12@gmail.com>
2026-03-18 19:44:52 +0000

    push swap

--- parce.c
+++ parce.c
@@ -1,2 +1,22 @@
 #include "push_swap.h"
 
+/*
+The parser needs to check 3 things:
+✅ Only digits (and maybe a - or + sign at the start)
+✅ No duplicates
+✅ No integer overflow (number fits in an int)
+*/
+
+int	ft_is_valid_number(char *str)
+{
+	int i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
